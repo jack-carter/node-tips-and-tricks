9a002fa7aba8c4f58b86a5cc1a6249a4416ff57e
Jack Carter <jack.carter@shinobigroup.com>
2020-06-29 23:00:12 -0500

    FUNCTION sequence is now working properly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-console.log("\nJavascript Throttle Demonstration\n")
-
 // we'll use old-style require() to allow use with versions
 // of Node.js prior to 13.
 const Throttle = require('./throttle')
+
+// We're going to capture activity in a history object, so 
+// we can collect activity streams separately for each demo
+// ensuring when we print them out they are not intertwined.
+function History(tagline) {
+    this.tagline = tagline
+    this.history = []
+
+    function save(msg) {
+        this.history.push(msg)
+    }
+}
 
 // A console.log replacement to include a timestamp
 const CONSOLE = (msg) => console.log(`[${new Date().toISOString()}] ${msg}`)
@@ -19,59 +29,52 @@ function WAIT(millis) {
 function demoOf(tagline,demo) {
     CONSOLE(`${tagline} demonstration`)
 
-    const MSG = (msg) => `${msg} --${tagline}--`
-
     // we should see all 3 of these
-    demo.immediate(MSG('immediately #1'))
-    demo.immediate(MSG('immediately #2'))
-    demo.immediate(MSG('immediately #3'))
+    demo.immediate(`immediate #1`)
+    demo.immediate(`immediate #2`)
+    demo.immediate(`immediate #3`)
 
-    // we should see only 1 of these
-    demo.debounced(MSG('debounce #1'))
-    demo.debounced(MSG('debounce #2'))
-    demo.debounced(MSG('debounce #3'))
+    // we should only see the last one of these
+    demo.debounced(`debounced #1`)
+    demo.debounced(`debounced #2`)
+    demo.debounced(`debounced #3`)
 
     WAIT(1000).then(() => {
-        // we should see only 2 of these
-        let regulator = { count: 0, timeout: null }
-        regulator.timeout = setInterval(() => demo.regulated(MSG(`regulator #${++regulator.count}`)), 100)
-        setTimeout(() => clearInterval(regulator.timeout), 1000)    
-    })
-
-    WAIT(5000).then(() => {
         // we should see all 9 of these, but over about a 4.5 second duration
         let interval = { count: 0, timeout: null }
-        interval.timeout = setInterval(() => demo.throttled(MSG(`throttle #${++interval.count}`)), 100)
+        interval.timeout = setInterval(() => demo.throttled(`throttled #${++interval.count}`), 100)
         setTimeout(() => clearInterval(interval.timeout), 1000)
+    })
+
+    WAIT(3000).then(() => {
+        // we should see only 2 of these
+        let regulator = { count: 0, timeout: null }
+        regulator.timeout = setInterval(() => demo.regulated(`regulated #${++regulator.count}`), 100)
+        setTimeout(() => clearInterval(regulator.timeout), 1000)    
     })
 }
 
 // Common construction of the demonstration object
 function DEMO(tagline,fn) {
-    WAIT(5000).then(() => {
-        demoOf(tagline,{
-            immediate: Throttle.immediate(fn),
-            debounced: Throttle.debounced(fn,500),
-            regulated: Throttle.regulated(fn,500),
-            throttled: Throttle.throttled(fn,500)
-        }) 
-    })
+    demoOf(tagline,{
+        immediate: Throttle.immediate(fn),
+        debounced: Throttle.debounced(fn,500),
+        regulated: Throttle.regulated(fn,500),
+        throttled: Throttle.throttled(fn,500)
+    }) 
 }
+
+console.log("\nJavascript Throttle Demonstration\n")
 
 // An object to test against
 let target = { 
     // using an object property ensures our call context is set properly
-    show(msg) { CONSOLE(`${msg} (${this.name})`) },
+    save(msg) { CONSOLE(`${msg} (${this.name})`) },
     name: 'target',
+    history: new History('METHOD')
 }    
-
-CONSOLE('Waiting 5 seconds ...')
-WAIT(5000).then(() => CONSOLE('5 seconds has elapsed'))
-
-CONSOLE('Waiting another 5 seconds ...')
-WAIT(5000).then(() => CONSOLE('another 5 seconds has elapsed'))
 
 // And now for some demonstrations
 DEMO('FUNCTION',CONSOLE)    
-DEMO('METHOD',Throttle.METHOD(target,'show'))
+//DEMO('METHOD',Throttle.METHOD(target,'save'))
 
